24fe90053fb971a5b38e1447aad80bc05db8b045
Ayrox <eweng@live.fr>
2022-02-01 18:13:42 +0100

    add electronBuilder config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+    },
+    electronBuilder: {
+      nodeIntegration: true,
+    },
   }
 }
