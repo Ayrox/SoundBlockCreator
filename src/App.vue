34fdff332fe6b48d3889667ed30f283b78069642
Ayrox <eweng@live.fr>
2022-02-07 20:51:57 +0100

    beginning project

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-toolbar right dark >
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      </v-toolbar-side-icon>
+      <v-toolbar-title>SoundBlock Creator</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn color="#555" href="#">About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-main>
+      <v-navigation-drawer
+        :mini-variant.sync="mini"
+        dark
+        left
+        v-model="drawer"
+      >
+        <v-list-item dense>
+          <v-list-item-content>
+            <v-list-item-avatar>
+              <v-img src="./assets/logo.svg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-subtitle>
+              Create your own Sound Pack for Minecraft
+            </v-list-item-subtitle>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
       <router-view />
     </v-main>
   </v-app>
@@ -43,8 +55,17 @@
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  }),
+  data: () => {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      drawer: true,
+      mini: true,
+
+    };
+  },
 };
 </script>
