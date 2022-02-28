31c2f1dd045b78af1c927b08e067336955609f44
Ayrox <eweng@live.fr>
2022-02-28 20:19:37 +0100

    Added a search bar

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    {{ action.name }}
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    action: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -3,20 +3,31 @@
 <template>
   <div>
     <PageLoader v-if="!isloaded" />
+    <v-text-field
+      v-model="search"
+      clearable
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="Search"
+    />
     <v-lazy>
       <div>
-        <v-container v-for="(type, t) in types" :key="t" fluid>
-          <v-subheader>{{ type.name.toUpperCase() }}</v-subheader>
+        <v-container v-for="(type, t) in searchType" :key="t" fluid>
+          <v-subheader>{{
+            type.name.toUpperCase().replace(["_"], " ")
+          }}</v-subheader>
           <v-expansion-panels>
             <v-expansion-panel v-for="(object, o) in type.objects" :key="o">
               <v-expansion-panel-header>
-                {{ object.name.toUpperCase().replace("_", " ") }}
+                {{ object.name.toUpperCase().replace(["_"], " ") }}
               </v-expansion-panel-header>
               <v-expansion-panel-content
                 v-for="(action, a) in object.actions"
                 :key="a"
               >
-                <v-card> {{ action.name }} </v-card>
+                <Card :action="action" />
               </v-expansion-panel-content>
             </v-expansion-panel>
           </v-expansion-panels>
@@ -29,6 +40,7 @@
 <script>
 import default_sounds from "../utils/DefaultSounds.json";
 import PageLoader from "../components/PageLoader.vue";
+import Card from "../components/Card.vue";
 
 function getInfo() {
   var types = [];
@@ -68,23 +80,57 @@ function getInfo() {
         (object) => object.name == sound_splitted[1]
       )[0];
     }
-    
+
     object.actions.push({
       name: action,
     });
   });
-  console.log(types);
   return types;
 }
 
 export default {
   data: () => ({
+    search: "",
     isloaded: false,
     types: getInfo(),
   }),
 
   components: {
     PageLoader,
+    Card,
+  },
+  computed: {
+    searchType() {
+      let se;
+      if (this.search !== "" && !this.search.startsWith(" ")) {
+        se = getInfo();
+
+        se.map((type) => {
+          type.objects = type.objects.filter((object) =>
+            object.name.toLowerCase().includes(this.search.toLowerCase())
+          );
+        });
+
+        se = se.filter(
+          (type) =>
+            type.name.toLowerCase().includes(this.search.toLowerCase()) ||
+            type.objects.length > 0
+        );
+        if (
+          se.filter(
+            (type) => type.name.toLowerCase() === this.search.toLowerCase()
+          ).length > 0
+        ) {
+          se = getInfo().filter(
+            (type) => type.name.toLowerCase() === this.search.toLowerCase()
+          );
+        }
+      } else {
+        se = this.types;
+      }
+      //console.log(se);
+      return se;
+    },
   },
 };
 </script>
