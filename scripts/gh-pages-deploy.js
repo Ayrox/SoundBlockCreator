b230dfd55d29ae349663adb4baa430d15613609a
Ayrox <eweng@live.fr>
2022-03-07 12:42:43 +0100

    Revert "oui"
    This reverts commit 6d518a039df19f9f8148a7070c1abdac31021892.

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,7 +3,7 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    //await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
@@ -13,7 +13,7 @@ const fs = require("fs");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    //await execa("rm", ["-r", folderName]);
+    await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
